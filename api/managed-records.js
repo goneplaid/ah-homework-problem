f47ed9b24d0c7ab4670784a1da4cddf51f895b1d
Shawn Wilson <wilson.ohyeah@gmail.com>
2021-02-12 13:23:50 -0700

    On second though, don't inline all of the functions used by retrieve. Instead, declare them after the default export of retrieve(). They do not need to be included in the exported function. Gross.

--- api/managed-records.js
+++ api/managed-records.js
@@ -15,74 +15,72 @@ async function retrieve(params = {}) {
   }
 
   return renderPayload(records, config);
-
-  // Internal functions
-
-  function buildConfig(params, primaryColors) {
-    const LIMIT = 10;
-
-    const page = params.page || 1;
-    const offset = page === 1 ? 0 : (page - 1) * LIMIT;
-    const colorFilter = params.colors || [];
-
-    return {
-      page,
-      limit: LIMIT,
-      offset,
-      colorFilter,
-      primaryColors,
-    };
-  }
-
-  async function fetchRecords(config) {
-    const response = await fetch(buildUri(config));
-
-    let records = [];
-
-    try {
-      records = await response.json();
-    } catch (error) {
-      console.log(error);
-    }
-
-    return records;
-  }
-
-  function renderPayload(recordsPlusOne, config) {
-    const records = recordsPlusOne.slice(0, config.limit);
-    const openRecords = records.filter(r => r.disposition === 'open');
-    const closedRecords = records.filter(r => !openRecords.includes(r));
-
-    const ids = records.map(r => r.id);
-    const open = openRecords.map((r) => ({ ...r, isPrimary: config.primaryColors.includes(r.color) }));
-    const closedPrimaryCount = closedRecords.filter(r => config.primaryColors.includes(r.color)).length;
-
-    const hasPreviousPage = config.page > 1;
-    const hasNextPage = !(recordsPlusOne.length < config.limit + 1);
-
-    const previousPage = hasPreviousPage ? config.page - 1 : null;
-    const nextPage = hasNextPage ? config.page + 1 : null;
-
-    return {
-      ids,
-      open,
-      closedPrimaryCount,
-      previousPage,
-      nextPage,
-    };
-  }
-
-  function buildUri(config = {}) {
-    // always grab one extra to determine if this is the last page or not
-    const queryParams = {
-      limit: config.limit + 1,
-      offset: config.offset,
-    };
-
-    if (config.colorFilter.length) queryParams['color[]'] = config.colorFilter;
-
-    return URI(window.path).query(queryParams).toString();
-  }
 }
 
 export default retrieve;
+
+function buildConfig(params, primaryColors) {
+  const LIMIT = 10;
+
+  const page = params.page || 1;
+  const offset = page === 1 ? 0 : (page - 1) * LIMIT;
+  const colorFilter = params.colors || [];
+
+  return {
+    page,
+    limit: LIMIT,
+    offset,
+    colorFilter,
+    primaryColors,
+  };
+}
+
+async function fetchRecords(config) {
+  const response = await fetch(buildUri(config));
+
+  let records = [];
+
+  try {
+    records = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+
+  return records;
+}
+
+function renderPayload(recordsPlusOne, config) {
+  const records = recordsPlusOne.slice(0, config.limit);
+  const openRecords = records.filter(r => r.disposition === 'open');
+  const closedRecords = records.filter(r => !openRecords.includes(r));
+
+  const ids = records.map(r => r.id);
+  const open = openRecords.map((r) => ({ ...r, isPrimary: config.primaryColors.includes(r.color) }));
+  const closedPrimaryCount = closedRecords.filter(r => config.primaryColors.includes(r.color)).length;
+
+  const hasPreviousPage = config.page > 1;
+  const hasNextPage = !(recordsPlusOne.length < config.limit + 1);
+
+  const previousPage = hasPreviousPage ? config.page - 1 : null;
+  const nextPage = hasNextPage ? config.page + 1 : null;
+
+  return {
+    ids,
+    open,
+    closedPrimaryCount,
+    previousPage,
+    nextPage,
+  };
+}
+
+function buildUri(config = {}) {
+  // always grab one extra to determine if this is the last page or not
+  const queryParams = {
+    limit: config.limit + 1,
+    offset: config.offset,
+  };
+
+  if (config.colorFilter.length) queryParams['color[]'] = config.colorFilter;
+
+  return URI(window.path).query(queryParams).toString();
+}
